c9c40d0533b80206d6f8368c231d1869cae05883
Marco Antonio <marco.ag.pegoraro@hotmail.com>
2019-12-22 22:14:57 -0300

    mudado arquivo fornecedor.yml

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -3,6 +3,7 @@ package br.com.alura.microservice.loja.service;
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,11 +18,19 @@ public class CompraService {
     @Autowired
     private RestTemplate client;
 
+    @Autowired
+    private DiscoveryClient eurekaClient;
+
     public void realizaCompra(CompraDTO compraDTO) {
 
         ResponseEntity<InfoFornecedorDTO> exchange =
                 client.exchange("http://fornecedor/info/"+compraDTO.getEndereco().getEstado()
         , HttpMethod.GET, null, InfoFornecedorDTO.class);
+
+        eurekaClient.getInstances("fornecedor").stream()
+                .forEach(fornecedor -> {
+                    System.out.println("localhost"+fornecedor.getPort());
+                });
 
         System.out.println(exchange.getBody().getEndereco());
     }
